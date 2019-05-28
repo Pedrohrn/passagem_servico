1b0a8ea8808cad05e789c97185316a5751d3be01
Pedrohrn <dev14.seucondominio@gmail.com>
2019-05-28 17:41:52 -0300

    criação de pessoas

---
+++ app/models/pessoa.rb
@@ -0,0 +1,6 @@
+class Pessoa < ApplicationRecord
+	validates_presence_of :nome, message: "Nome deve ser preenchido!"
+
+	has_many :passagem_servicos, foreign_key: 'pessoa_entrou_id'
+	# has_many :passagem_servicos, foreign_key: 'pessoa_saiu_id'
+end

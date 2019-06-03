d62ffb8eaf9ddebcfa8717afcb253f63123c76e1
Pedrohrn <dev14.seucondominio@gmail.com>
2019-06-03 11:55:27 -0300

    liberando metódos para o perfil

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'passagem_servicos#index'
 
-  resources :passagem_servicos, only: [:index, :create, :show, :destroy, :edit] #precisa liberar os métodos aqui
+  resources :passagem_servicos, only: [:index, :create, :show, :destroy, :update] #precisa liberar os métodos aqui
   resources :pessoas, only: [:index]
-  resources :categorias, only: [:index, :create, :destroy, :edit]
+  resources :categorias, only: [:index, :create, :destroy, :update]
+  resources :perfis, only: [:index, :create, :destroy, :update]
 end
